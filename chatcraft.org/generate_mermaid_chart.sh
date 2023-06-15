ea3193986659df14de095a3b22f7a7c0bb61f278
Steven Li <steven4354@gmail.com>
2023-06-15 13:18:29 -0700

    Update generate_mermaid_chart.sh

--- chatcraft.org/generate_mermaid_chart.sh
+++ chatcraft.org/generate_mermaid_chart.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <relative_path>"
+  exit 1
+fi
+
+RELATIVE_PATH="$1"
 OUTPUT_FILE="mermaid_chart.md"
 
 echo "graph TD" > $OUTPUT_FILE
 
-tree -fi --noreport -I 'node_modules|.*|*.json|*build*|*dist*|public|assets|*config*' | while read -r line; do
-  parent=$(dirname "$line")
-  child=$(basename "$line")
-  parent_id="${parent//\//-}"
-  child_id="${child//\//-}"
-  echo "${parent_id}[${parent}] --> ${child_id}[${child}]" >> $OUTPUT_FILE
+tree -fi --noreport -I 'node_modules|.*|*.json|*build*|*dist*|public|assets|*config*' "$RELATIVE_PATH" | while read -r line; do
+  if [ -f "$line" ]; then
+    parent=$(dirname "$line")
+    child=$(basename "$line")
+    parent_id="${parent//\//-}"
+    child_id="${parent//\//-}-${child//\//-}"
+    echo "${parent_id}[${parent}] --> ${child_id}[${child}]" >> $OUTPUT_FILE
+  fi
 done
 
 echo "Mermaid chart generated in $OUTPUT_FILE"
