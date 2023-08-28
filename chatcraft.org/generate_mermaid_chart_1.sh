15e2a051129372c71547f3a66cd926688ecc88b6
Steven Li <steven4354@gmail.com>
2023-08-28 16:11:10 -0700

    Create generate_mermaid_chart_1.sh

---
+++ chatcraft.org/generate_mermaid_chart_1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# seems to get just the folders
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <relative_path>"
+  exit 1
+fi
+
+RELATIVE_PATH="$1"
+OUTPUT_FILE="mermaid_chart.md"
+
+echo "graph TD" > $OUTPUT_FILE
+
+tree -fi --noreport -I 'node_modules|.*|*.json|*build*|*dist*|public|assets|*config*' "$RELATIVE_PATH" | while read -r line; do
+  parent=$(dirname "$line")
+  child=$(basename "$line")
+  parent_id="${parent//\//-}"
+  child_id="${parent//\//-}-${child//\//-}"
+  parent_id="${parent_id//./_}"
+  child_id="${child_id//./_}"
+  parent_id="${parent_id//-/_}"
+  child_id="${child_id//-/_}"
+  echo "${parent_id}[${parent}] --> ${child_id}[${child}]" >> $OUTPUT_FILE
+done
+
+echo "Mermaid chart generated in $OUTPUT_FILE"
